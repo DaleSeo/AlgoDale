82b411ac8061d0c464e60f1635d89d0d842a243a
Dale Seo <dale.seo@gmail.com>
2023-05-21 13:23:21 -0400

    footer log social

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import prettier from "prettier-plugin-astro";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  plugins: [prettier],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 });
