3b99f3f7ca5176d76ade1d092135d7a1cb1661a8
Dale Seo <dale.seo@gmail.com>
2023-05-28 22:38:59 -0400

    use astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind()],
   plugins: [prettier],
   overrides: [{
     files: "*.astro",
