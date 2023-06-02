4df04fca2954276d6f852dabbd742f0f40ce23ea
Dale Seo <dale.seo@gmail.com>
2023-06-02 07:26:42 -0400

    tw colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00ebc7",
+        secondary: "#ff526f",
+        tertiary: "#9be688",
+        background: "#f3f5f7",
+        text: "#001126",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
