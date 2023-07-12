e2f963b11e9677385f96806464b860d93d3cf1ab
Dale Seo <dale.seo@gmail.com>
2023-07-11 21:27:57 -0400

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "알고달레";
-export const SITE_DESCRIPTION =
-  "달레가 코딩 테스트를 위한 알고리즘 공부를 도와드릴께요";
+export const SITE_DESCRIPTION = "코딩 테스트 준비를 위한 첫걸음";
