8efacd8ce3862dd33eec6df0390a8c4a11fae6e0
Dale Seo <dale.seo@gmail.com>
2023-07-15 16:53:30 -0400

    other websites

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "알고달레";
-export const SITE_DESCRIPTION = "코딩 테스트 준비를 위한 첫걸음";
+export const SITE_DESCRIPTION = "알고리즘 입문자를 위한 달레의 친절한 가이드";
