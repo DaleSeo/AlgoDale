72f4f73a82343d0ca630c2a03c1166ccdf56ea1a
Dale Seo <dale.seo@gmail.com>
2024-02-03 21:20:33 -0500

    spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,17 @@ import partytown from "@astrojs/partytown";
 // import rehypeSlug from "rehype-slug";
 // import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.algodale.com/",
   scopedStyleStrategy: "where",
   trailingSlash: "always",
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown(), sentry(), spotlightjs()],
   // markdown: {
   //   rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
   // },
-  plugins: [prettier],
+  plugins: [prettier]
 });
